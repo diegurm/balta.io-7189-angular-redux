aa7597552cd94dc40f4798181be899247eafee30
Diego Rodrigues <diegurm@hotmail.com>
2019-10-13 00:20:30 -0300

    Atualizado DataService

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,18 +1,18 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ProductModel } from './models/product.model';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root'
+})
 export class DataService {
+  public url = 'http://localhost:7189/v1';
 
-    public url = 'http://localhost:7189/v1';
+  constructor(
+    private http: HttpClient
+  ) { }
 
-    constructor(private http: HttpClient){
-
-    }
-
-
-    getProducts() {
-        return this.http.get(`${this.url}/products`);
-    }
+  getProducts() {
+    return this.http.get<ProductModel[]>(`${this.url}/products`);
+  }
 }
-
